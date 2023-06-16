2730062ab84404ac682727e9b681f5b90fd0dcdd
SirBeho <benjamin.tavarez.98@gmail.com>
2023-06-16 19:53:17 -0400

    Fondos dinamicos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import "./App.css";
-import React, { useState } from "react";
+import React, { useEffect, useState, version } from "react";
 import Card from "./components/Card/Card";
 import Datos from "./components/Datos";
 import Desplegar from "./components/Desplegar";
 
-
 function App() {
   const [empleados, setEmpleados] = useState(Datos);
-   const modificar = (cartas) => {
-     setEmpleados(cartas)
+  const [background, setBackground] = useState("red");
+  
+   const modificar = (cartas,backgroundImage) => {
+      setBackground(backgroundImage);
+      setEmpleados(cartas);
     }
+    useEffect(() => {
+      document.body.style.backgroundImage = background;
+    }, [background]);
+    
   return (
     <>
       <Desplegar desplegar={modificar} />
--- src/components/Desplegar.jsx
+++ src/components/Desplegar.jsx
@@ -1,23 +1,51 @@
 import Datos from "./Datos";
+import React, { useEffect, useState, version } from "react";
 
 export default function Desplegar({ desplegar }) {
 
-
+  const [background, setBackground] = useState("");
   function actualizar(e){
+
        e.preventDefault();
       const form = document.getElementById("form");
 
+
       const deporte = Datos[form.deporte.value];
+      let badground = "";
+      
+      switch (parseFloat(form.deporte.value)) {
+        case 0:
+          badground = 'url("https://img.freepik.com/free-vector/hand-drawn-flat-basketball-logo_52683-84654.jpg")';
+            
+          break;
+        case 1:
+          badground = 'url("https://img.freepik.com/vector-premium/logotipo-club-futbol_441059-74.jpg")';
+          break;
+          
+        case 2:
+          badground = 'url("https://cdn.domestika.org/c_limit,dpr_auto,f_auto,q_auto,w_820/v1415399156/content-items/001/127/065/LOL-original.jpg?1415399156")';
+          break;
+      
+        default:
+          badground = "black";
+          break;
+      
+        }
+        setBackground(badground);
+
+       
       const new_cards = [];
         for (let index = 0; index < 3; index++) {
         
           new_cards.push(deporte[Math.floor( Math.random() * deporte.length)]);
         }
-        desplegar(new_cards)
+        desplegar(new_cards,badground)
+
   }
   
   return (
-    <form id="form"  className="principal">
+    <form id="form"  className="principal" 
+    style={{backgroundImage :background}}>
       <label>  Deporte
         <select name="deporte" id="sexo-alumno" required>
           <option value=""></option>
